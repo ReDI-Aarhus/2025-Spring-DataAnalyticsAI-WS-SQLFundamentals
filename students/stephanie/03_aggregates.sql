3f1e2908ce8984255b6f1fe6fb99b31b4054cc62
Stephers <stephsegawhite@gmail.com>
2025-05-07 19:15:37 +0200

    Add files, complete exercise 01

---
+++ students/stephanie/03_aggregates.sql
@@ -0,0 +1,35 @@
+--=======================================
+-- Exercise 03: aggregates
+--=======================================
+-- Topic: GROUP BY and Aggregation
+-- Task: Show the number of orders per customer (only those who placed at least 1 order).
+-- Hint: Use GROUP BY CustomerID and COUNT(*)
+--=======================================
+-- Your query here
+
+
+--=======================================
+-- Topic: SUM + Aliases and Formatting
+-- Task: Show the number of orders per customer AND total sales amount
+-- Hint: Use "SUM" and use "AS" to rename columns.
+--=======================================
+-- Your query here
+
+
+--=======================================
+-- Topic: Aliases and Formatting
+-- Task: Display product names and prices, rename the columns to "Product" and "Price".
+-- Hint: Use "AS" to rename columns
+--=======================================
+-- Your query here
+
+
+--=======================================
+-- Topic: Filter with HAVING
+-- Task: Get colors where average list price > 500
+-- Hint: Use "AVG" both in SELECT and in filter
+--=======================================
+-- Your query here
+
+
+--=======================================
