a20d3490eb6ca2c5d30a43a821d4db743ea70825
Stephers <stephsegawhite@gmail.com>
2025-05-07 19:15:37 +0200

    Add files, complete exercise 01

---
+++ students/stephanie/01_select_basics.sql
@@ -0,0 +1,63 @@
+--=======================================
+-- Exercise 01: SELECT basics
+--=======================================
+-- Topic: SELECT Basics
+-- Task: : Select your name
+-- Hint: Use '' (single quotes) for text e.g. 'Joe'
+--=======================================
+
+SELECT 'Stephanie' AS Name;
+
+
+--=======================================
+-- Topic: SELECT Basics
+-- Task: : Calculate 599 + 1
+-- Hint: 
+--=======================================
+
+SELECT 599 + 1 AS Result;
+
+--=======================================
+-- Topic: SELECT Basics
+-- Task: : Select all columns from the Product table
+-- Hint: Use SELECT *
+--=======================================
+
+SELECT * 
+FROM SalesLT.Product;
+
+-- Make sure this is from the correct database!
+
+--=======================================
+-- Topic: SELECT Basics
+-- Task: Retrieve the FirstName, LastName, and EmailAddress of all customers.
+-- Hint: Use SELECT with FROM SalesLT.Customer
+--=======================================
+
+SELECT FirstName, LastName, EmailAddress FROM SalesLT.Customer;
+
+--=======================================
+-- Topic: Using Distinct
+-- Task: Find all unique products colors
+-- Hint: Use DISTINCT
+--=======================================
+
+SELECT DISTINCT Color FROM SalesLT.Product;
+
+--=======================================
+-- Topic: Filtering with WHERE
+-- Task: Find all products that cost more than $1,000.
+-- Hint: Use WHERE with ListPrice
+--=======================================
+
+SELECT * FROM SalesLT.Product WHERE ListPrice > 1000;
+
+--=======================================
+-- Topic: Sorting with ORDER BY
+-- Task: List the top 10 most expensive products
+-- Hint: Use ORDER BY (DESC) + TOP clause
+--=======================================
+
+SELECT TOP 10 * FROM SalesLT.Product ORDER BY ListPrice DESC;
+
+--=======================================
