6244d2a89864d7b5a9c38e4e10263d07945068c1
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-05-07 19:53:37 +0200

    complete slect exercise

---
+++ exercises/Students/01_select_basics.sql
@@ -0,0 +1,71 @@
+--=======================================
+-- Exercise 01: SELECT basics
+--=======================================
+-- Topic: SELECT Basics
+-- Task: : Select your name
+-- Hint: Use '' (single quotes) for text e.g. 'Joe'
+--=======================================
+-- Your query here
+USE ReDI-SQL-Fundamentals-01;
+SELECT 'guzide' AS Name;
+
+
+
+--=======================================
+-- Topic: SELECT Basics
+-- Task: : Calculate 599 + 1
+-- Hint: 
+--=======================================
+-- Your query here
+SELECT 599+1;
+
+
+--=======================================
+-- Topic: SELECT Basics
+-- Task: : Select all columns from the Product table
+-- Hint: Use SELECT *
+--=======================================
+-- Your query here
+SELECT * 
+FROM [SalesLT].[Product];
+
+--=======================================
+-- Topic: SELECT Basics
+-- Task: Retrieve the FirstName, LastName, and EmailAddress of all customers.
+-- Hint: Use SELECT with FROM SalesLT.Customer
+--=======================================
+-- Your query here
+SELECT FirstName, LastName, FirstName+''+LastName AS 'FullName', EmailAddress
+FROM SalesLT.Customer;
+
+--=======================================
+-- Topic: Using Distinct
+-- Task: Find all unique products colors
+-- Hint: Use DISTINCT
+--=======================================
+-- Your query here
+SELECT DISTINCT Color
+FROM SalesLT.Product;
+
+--=======================================
+-- Topic: Filtering with WHERE
+-- Task: Find all products that cost more than $1,000.
+-- Hint: Use WHERE with ListPrice
+--=======================================
+-- Your query here
+SELECT ProductID, p.name AS ProductName, ListPrice
+FROM SalesLT.Product AS p
+WHERE ListPrice > 1000;
+
+--=======================================
+-- Topic: Sorting with ORDER BY
+-- Task: List the top 10 most expensive products
+-- Hint: Use ORDER BY (DESC) + TOP clause
+--=======================================
+-- Your query here
+SELECT TOP 10 ProductID, p.Name AS ProductName, ListPrice
+FROM SalesLT.Product AS p
+ORDER BY ListPrice DESC;
+
+
+--=======================================
