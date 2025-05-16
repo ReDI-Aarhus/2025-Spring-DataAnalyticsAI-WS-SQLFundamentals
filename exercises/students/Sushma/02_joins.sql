50c2f4090632d827b68486a4329298416c0992b2
Sushma Budhathoki <bsushma047@gmail.com>
2025-05-16 23:05:17 +0200

    Exercise Joins finished

--- exercises/students/Sushma/02_joins.sql
+++ exercises/students/Sushma/02_joins.sql
@@ -6,6 +6,12 @@
 -- Hint: [SalesLT].[Product].[ProductCategoryID] + [SalesLT].[ProductCategory].[ProductCategoryID]
 --=======================================
 -- Your query here
+SELECT SalesLT.Product.Name, SalesLT.ProductCategory.Name
+FROM SalesLT.Product
+    INNER JOIN SalesLT.ProductCategory
+    ON SalesLT.Product.ProductCategoryID = SalesLT.ProductCategory.ProductCategoryID
+------I am confused here, what columns to select. I selected the name of both tables.      
+
 
 
 --=======================================
@@ -14,6 +20,11 @@
 -- Hint: LEFT JOIN with SalesOrderHeader
 --=======================================
 -- Your query here
+SELECT SalesLT.Customer.FirstName, SalesLT.SalesOrderHeader.SalesOrderID
+FROM SalesLT.Customer
+    LEFT JOIN SalesLT.SalesOrderHeader
+    ON SalesLT.Customer.CustomerID = SalesLT.SalesOrderHeader.CustomerID
+
 
 
 --=======================================
