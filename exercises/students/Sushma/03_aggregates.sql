05dcbfc777b42818698d0dd76003b08e6db532d1
Sushma Budhathoki <bsushma047@gmail.com>
2025-05-17 14:29:12 +0200

    Exercises from aggregtes finished

--- exercises/students/Sushma/03_aggregates.sql
+++ exercises/students/Sushma/03_aggregates.sql
@@ -6,6 +6,10 @@
 -- Hint: Use GROUP BY CustomerID and COUNT(*)
 --=======================================
 -- Your query here
+SELECT COUNT(SalesOrderID) AS NumberOfOrders, CustomerID
+FROM SalesLT.SalesOrderHeader
+GROUP BY CustomerID
+
 
 
 --=======================================
@@ -14,15 +18,19 @@
 -- Hint: Use "SUM" and use "AS" to rename columns.
 --=======================================
 -- Your query here
-
+SELECT SUM(TotalDue) AS TotalSales, COUNT(SalesOrderID) AS NumberOfOrders, CustomerID
+FROM SalesLT.SalesOrderHeader
+GROUP BY CustomerID
 
 --=======================================
 -- Topic: Aliases and Formatting
 -- Task: Display product names and prices, rename the columns to "Product" and "Price".
 -- Hint: Use "AS" to rename columns
---=======================================
+--=======================================  
 -- Your query here
-
+SELECT Name AS Product, ListPrice AS Price
+FROM SalesLT.Product
+--here table name is not mentioned?
 
 --=======================================
 -- Topic: Filter with HAVING
@@ -30,6 +38,9 @@
 -- Hint: Use "AVG" both in SELECT and in filter
 --=======================================
 -- Your query here
-
+SELECT Color, AVG(ListPrice) AS AverageListPrice
+FROM SalesLT.Product
+GROUP BY Color
+HAVING AVG(ListPrice) > 500
 
 --=======================================
