1afb590e4360af2fed0baad15e1ad863903be018
Magda <mklimczak05@gmail.com>
2025-05-20 13:44:57 +0200

    Completed 02-joins exercise

--- exercises/students/magda/02_joins.sql
+++ exercises/students/magda/02_joins.sql
@@ -6,14 +6,19 @@
 -- Hint: [SalesLT].[Product].[ProductCategoryID] + [SalesLT].[ProductCategory].[ProductCategoryID]
 --=======================================
 -- Your query here
-
-
+SELECT p.Name AS ProductName, c.Name AS CategoryName
+FROM SalesLT.Product p
+INNER JOIN SalesLT.ProductCategory c
+ON p.ProductCategoryID = c.ProductCategoryID;
 --=======================================
 -- Topic: LEFT JOIN Practice
 -- Task: List all customers with order IDs, even those who have never placed an order.
 -- Hint: LEFT JOIN with SalesOrderHeader
 --=======================================
 -- Your query here
-
+SELECT c.CustomerID, c.FirstName, c.LastName, o.SalesOrderID
+FROM SalesLT.Customer c
+LEFT JOIN SalesLT.SalesOrderHeader o
+ON c.CustomerID = o.CustomerID;
 
 --=======================================
